c62543a617c0bc82bde3231c7085d43c1d9f64a5
Jeremy Kemp <jekemp@davidson.edu>
2023-11-15 11:20:55 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,27 @@ function logSubmit(event) {
 const form = document.getElementById("infoForm");
 const mainBody = document.getElementById("mainBod");
 form.addEventListener("submit", logSubmit);
+
+
+
+//Get users location
+
+
+
+
+
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+		{
+			headers: { Authorization: "Bearer {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}" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+query({"inputs": "Astronaut riding a horse"}).then((response) => {
+	// Use image
+});
