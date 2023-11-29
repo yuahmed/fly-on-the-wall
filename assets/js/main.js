c053793ca9b430d57010eda7dfea4031c12d5aa9
Jeremy Kemp <jekemp@davidson.edu>
2023-11-28 19:11:55 -0500

    V1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,18 @@
-// This is the main JavaScript file that will handle navigation and page loading.
-
 // Function to load the form page
-function loadForm() {
+async function loadForm() {
   // Use history.pushState to change the URL without reloading the page
   history.pushState({ page: 'form' }, 'Form Page', 'form.html');
   
   // Load the content of the form page into the #app div
-  document.getElementById('app').innerHTML = fetchContent('form.html');
+  document.getElementById('app').innerHTML = await fetchContent('form.html');
+
+  
 }
 
 // Function to submit the form and load the images page
 function submitForm(event) {
   event.preventDefault();
-  // Process your form data here
+  // Process form data here
   
   // Use history.pushState to change the URL without reloading the page
   history.pushState({ page: 'images' }, 'Images Page', 'images.html');
@@ -21,9 +21,10 @@ function submitForm(event) {
   document.getElementById('app').innerHTML = fetchContent('images.html');
 }
 
+
 // Function to show the images
 function showImages() {
-  // Your logic to display images goes here
+  // logic to display images goes here
   
   // Use history.pushState to change the URL without reloading the page
   history.pushState({ page: 'about' }, 'About Page', 'about.html');
@@ -32,16 +33,11 @@ function showImages() {
   document.getElementById('app').innerHTML = fetchContent('about.html');
 }
 
-// Function to fetch the content of a page using AJAX or Fetch API
-function fetchContent(page) {
-  // Your logic to fetch content goes here
-  // You can use AJAX or Fetch API to get the content of the specified page
-  // For simplicity, you can use a synchronous XMLHttpRequest
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', page, false);
-  xhr.send(null);
-  return xhr.responseText;
+async function fetchContent(url) {
+	const response = await fetch(url);
+	return await response.text();
 }
+
 
 // Event listener for the browser back button
 window.onpopstate = function(event) {
